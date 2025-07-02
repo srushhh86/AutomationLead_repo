9b6a5f3dde351017b6082402c184f059f4d90074
DELL <DELL@DESKTOP-GJGDPEI>
2025-07-02 13:20:21 +0530

    Added Kawasaki

---
+++ src/test/java/Kawasaki.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Kawasaki 
+{
+
+	@Test
+	public void launch()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.kawasaki-india.com/");
+		Reporter.log("Baja Launched",true);
+	}
+
+}
